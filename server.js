22295a535028fc750866e5dffd166c0f603f1b50
Pawan Kumar <surpawan@gmail.com>
2020-11-22 17:43:58 +0530

    Added Express Server and EJS templating engine

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Homepage Route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Set Template Engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
